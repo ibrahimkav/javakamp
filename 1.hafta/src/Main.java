6272cfd503c82ab8bfebf502e4f77917d49cf649
İbrahim Kav <ibrahimkaav@gmail.com>
2022-06-02 02:43:07 +0300

    first

---
+++ 1.hafta/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//camelCase
+		// Don't repeat yourself
+		String internetSubeButonu="internet subesi";
+		System.out.println(internetSubeButonu);
+	    // System.out.println("internet subeye gir");
+		// System.out.println("internet subeye gir");
+		// System.out.println("internet subeye gir");
+	}
+
+}
